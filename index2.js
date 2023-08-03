1f349bb060344b9ced0ff1cbbfbf3aa1d65c4475
Kostin Ilya <ilyaspecial999@gmail.com>
2023-08-04 01:46:03 +0300

    upd

--- index2.js
+++ index2.js
@@ -4,13 +4,54 @@ const server = require('http').createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let backend = null;
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+})
+
+app.get('/access', (req, res) => {
+    const data = req.query;
+    console.log('New request!');
+    console.log(data);
+
+    if(!backend) {
+        res.send({});
+        return;
+    }
+
+    res.send(data);
+    backend.emit('message', data);
+});
+
 
 io.on('connection', socket => {
-    socket.on('message', data => {
-        console.log(data);
-        socket.emit('message', {text: 'ХУУЙЙЙ'});
+    console.log('A new connection!');
+
+    if (backend) {
+        console.log('Backend has already connected.');
+        console.log('Disconnecting.');
+        socket.disconnect();
+        return;
+    }
+
+    socket.on('message', (msg) => {
+        sendData(msg.id, msg.data);
     });
-})
+
+    socket.on('test', (msg) => {
+        console.log('test msg:');
+        console.log(msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Backend has disconnected');
+        backend = null;
+    }) 
+
+    backend = socket;
+});
+
 
 server.listen(443, () => {
     console.log('listening on *:443');
